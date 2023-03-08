f4d6b86261b2bb9e6ebb370ed870104da5995f13
paula cavender <you@example.com>
2023-03-08 13:29:45 +0000

    contact box design

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,6 +3,32 @@ const Footer = () => {
     <footer id="contact">
       <div className="footer">
         <h3 className="title contact-title">contact me</h3>
+        <div className="contact-box">
+          <a
+            href="mailto:paula.cavender@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={"./assets/icons/email2.svg"} alt="email"></img>
+            <p>paula.cavender@gmail.com</p>
+          </a>
+          <a
+            href="https://github.com/paulajaynecavender"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={"./assets/icons/github2.svg"} alt="github"></img>
+            <p>paulajaynecavender</p>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/paulacavender/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={"./assets/icons/linkedin2.svg"} alt="linkedin"></img>
+            <p>paulacavender</p>
+          </a>
+        </div>
       </div>
     </footer>
   );
