e270dff863d9240cff1a0170c1ef395eaba571bc
paula cavender <you@example.com>
2023-03-21 21:54:49 +0000

    header size adjustmenrs

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header>
       <div className="header circle">
-        <div className="nav_brand ">
+        <div className="nav_brand">
           <h1>paula cavender</h1>
         </div>
         <div className="navbar-container">{/* <NavBar /> */}</div>
