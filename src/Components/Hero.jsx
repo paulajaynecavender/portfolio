192575eb4c86bbb197fef503ce7129d6b2d51f45
paula cavender <you@example.com>
2023-03-22 13:07:32 +0000

    hero section design updated

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,20 +2,18 @@ const Hero = () => {
   return (
     <section className="hero" id="hero">
       <div className="container">
-        <div className="hero-headline">
-          <div className="hero-headline-container">
-            <p>
-              Hi! My name is <h2 className="underline">paula cavender</h2> and I
-              am a <h2 className="underline">front-end developer</h2>
-            </p>
-          </div>
-          <div className="hero-img-container">
-            <img
-              className="hero-img"
-              src={"./assets/undraw_programmer_re_owql (1).svg"}
-              alt="illustration of programmer"
-            />
-          </div>
+        <div className="hero-headline-container">
+          <p>
+            Hi! My name is <h2 className="underline">paula cavender</h2> and I
+            am a <h2 className="underline">front-end developer</h2>
+          </p>
+        </div>
+        <div className="hero-img-container">
+          <img
+            className="hero-img"
+            src={"./assets/undraw_programmer_re_owql (1).svg"}
+            alt="illustration of programmer"
+          />
         </div>
         <p className="hero-description">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum,
