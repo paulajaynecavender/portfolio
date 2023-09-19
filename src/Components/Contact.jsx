8e758a4afd50a61c68234f7d90c02f3cf7e4bae7
paula cavender <you@example.com>
2023-09-19 14:58:49 +0100

    hover state css updates

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -4,34 +4,34 @@ const Contact = () => {
       <div className="container">
         <h3 className="title contact-title">Contact Me</h3>
         <div className="contact-btn-container">
-          <div className="contact-button ">
+          <div className="contact-button link">
             <a
               href="mailto:paula.cavender@gmail.com"
               target="_blank"
               rel="noopener noreferrer"
             >
               <img src={"./assets/icons/email3.svg"} alt="email"></img>
-              <p className="link">paula.cavender@gmail.com </p>
+              <p>paula.cavender@gmail.com </p>
             </a>
           </div>
-          <div className="contact-button ">
+          <div className="contact-button link">
             <a
               href="https://www.linkedin.com/in/paulacavender/"
               target="_blank"
               rel="noopener noreferrer"
             >
               <img src={"./assets/icons/linkedin3.svg"} alt="linkedin"></img>
-              <p className="link">linkedin.com/in/paulacavender </p>
+              <p>linkedin.com/in/paulacavender </p>
             </a>
           </div>
-          <div className="contact-button ">
+          <div className="contact-button link">
             <a
               href="https://github.com/paulajaynecavender"
               target="_blank"
               rel="noopener noreferrer"
             >
               <img src={"./assets/icons/github3.svg"} alt="github"></img>
-              <p className="link">github.com/paulajaynecavender</p>
+              <p>github.com/paulajaynecavender</p>
             </a>
           </div>
         </div>
